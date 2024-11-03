37ee2cbedb0343fd1fd5cb4c2189415d7066655c
Aleksandr-code <70668106+Aleksandr-code@users.noreply.github.com>
2024-11-03 21:29:42 +0500

    Create plugin/modal

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -1,10 +1,33 @@
 <script setup>
-  import ToDos from "@components/ToDos.vue"
+  import ToDos from "@components/ToDos.vue";
+  import {inject, ref} from "vue"
+
+  const $modals = inject("$modals"),
+  _result = ref("")
+
+  function showModal() {
+    _result.value = "";
+    $modals.show("myModal").then(() => {
+        _result.value = "Modal accepted"
+    }, () => {
+        _result.value = "Modal cancelled"
+    })
+}
+
 </script>
 
 <template>
   <div class="app w3-light-gray">
     <ToDos />
+    <div class="test">
+      <button @click="showModal">
+            Show modal
+      </button>
+      <div>{{_result}}</div>
+      <Modal name="myModal" title="Modal example">
+              Some important content here
+      </Modal>
+    </div>
   </div>
 </template>
 
